e7ad1637a1b6b986043b6335e3502f57231da1e6
Spooky <61633921+Spoookyyy@users.noreply.github.com>
2023-11-01 00:27:04 -0700

    new

--- quiz-prompter/app/page.tsx
+++ quiz-prompter/app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
 <main className="min-h-screen flex flex-col justify-between bg-black p-24">
 
-{/* Main Content */}
 <div className="flex flex-col items-center justify-center flex-grow animate-fadeInAnimation">
 
   <h1 
@@ -19,7 +18,6 @@ export default function Home() {
     Created by <Link href='https://github.com/Spoookyyy'>Spooky</Link>
   </p>
 
-  {/* Navigation */}
   <nav className="mb-8">
     <ul className="flex justify-center space-x-8">
       <li><Link href="https://github.com/Spoookyyy/QuestionPrompter" className='text-white text-lg transform hover:scale-105 transition-transform duration-300 hover:text-gray-500 transition animate-fadeInAnimation"'>Source</Link></li>
@@ -29,16 +27,12 @@ export default function Home() {
     </ul>
   </nav>
 
-  {/* Optional: Brief Introduction or Catchy Phrase */}
   <p className="text-white text-center mb-8">
-    {/* Content here */}
   </p>
 
 </div>
 
-{/* Footer */}
 <footer className="mt-8">
-  {/* Content here */}
 </footer>
 
 </main>
